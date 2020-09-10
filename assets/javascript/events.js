e65d39bf33687dc4c917a1c3a9ee8622edb4581c
sidporwal <siddharthaporwal9@gmial.com>
2020-09-10 14:43:27 +0530

    User deposit

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -1,1 +1,18 @@
+var note_2000=0,note_500=0,note_100=0,sum=0;
 
+
+function get_amount(){
+
+    note_2000 +=Number(document.getElementById("note_2000").value);
+    note_500 +=Number(document.getElementById("note_500").value);
+    note_100 +=Number(document.getElementById("note_100").value);
+    sum = (2000*note_2000) + (500*note_500) + (100*note_100);
+    document.getElementById("amount").innerHTML=
+    "Total 2000Rs. Note is :" + note_2000 +"<br>"+
+    "Total 500Rs. Note is :" + note_500 +"<br>"+
+    "Total 100Rs. Note is :" + note_100 +"<br>"+
+    "Total amount is:" + sum;
+    document.getElementById("note_2000").value=0;
+    document.getElementById("note_500").value=0;
+    document.getElementById("note_100").value=0;
+}
